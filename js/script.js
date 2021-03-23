14d5e9708b162ab3d0880356d12ef6a193827b77
Decilap denis <decilapdenis@gmail.com>
2021-03-23 10:59:44 +0100

    Ajout bandeaux en-tête

--- js/script.js
+++ js/script.js
@@ -1,38 +1,35 @@
 class User{
+    users = [];
     constructor(url){
-        this.users = [];
         _fetchData(url)
         .then((data) => {
             this.users = data.data;
             this.createUsers();
         }).catch((error) => {
-            console.log(error)
+            console.log(error);
         })
         .finally(() => {
             setTimeout(()=>{
-                document.querySelector('.users_cards').classList.remove('hide');
-                document.querySelector('.fa-spin').remove();
-            }, 1000);    
+                document.querySelector('.users .users_cards').classList.remove('hide');
+                document.querySelector('.users .fa-spin').remove();
+            }, 500);    
         });
     }
 
-
     createUser(user){
-        console.log(user.avatar)
-        let users = this.createDivElement('users_card', 'div');
-        let html = `<div class="avatar">
-                        <img src="${user.avatar}" alt="user avatar" class="users_avatar">
-                    </div>
-                    <div class="users_info">
-                        <h3>${user.first_name ? user.first_name + '' : ''}${user.last_name ? user.last_name : ''}</h3>
-                        <p>Lorem, ipsum.</p>
-                        <div class="card_network">
-                            <i class="fab fa-linkedin-in"></i>
-                            <i class="fab fa-facebook"></i>
-                            <i class="fab fa-twitter"></i>
-                        </div>
-                    </div>`;
-        users.innerHTML = html;
+        let users = this.createDivWithClass('users_card', 'div');
+        users.innerHTML = `<div class="avatar">
+                              <img src="${user.avatar}" alt="user avatar" class="users_avatar">
+                           </div>
+                           <div class="users_info">
+                                <h3>${user.first_name} ${user.last_name}</h3>
+                                <p>Lorem, ipsum.</p>
+                                <div class="card_network">
+                                    <i class="fab fa-linkedin-in"></i>
+                                    <i class="fab fa-facebook"></i>
+                                    <i class="fab fa-twitter"></i>
+                                </div>
+                           </div>`;
         return users;
     }
 
@@ -45,7 +42,7 @@ class User{
         });
     }
 
-    createDivElement(className){
+    createDivWithClass(className){
         let div = document.createElement('div');
         div.className = className;
         return div;
